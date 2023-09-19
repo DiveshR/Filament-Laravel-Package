e66578148de753510df02ea4788edbe0c6d50059
DiveshR <divesh20july@gmail.com>
2023-09-19 23:04:28 +0530

    change-color-font-family-logo-favicon-disable-dark-mode

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,12 @@ class AdminPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Fuchsia,
+                // 'primary' => '#cccccc'
             ])
+            ->font('fantasy')
+            ->favicon('https://laravel.com/img/logomark.min.svg')
+            // ->darkMode(true)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,2 @@
+<!-- Laravel Filament -->
+<img src="https://laravel.com/img/logomark.min.svg" class="h-10" >
